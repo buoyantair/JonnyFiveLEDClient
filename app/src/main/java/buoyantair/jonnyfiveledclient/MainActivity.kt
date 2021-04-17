fe0ac876cd363c4b7b5014259069d1c6b051fc96
Alice Kile <buoyantair@protonmail.com>
2021-04-17 22:42:14 +0530

    Add button

--- app/src/main/java/buoyantair/jonnyfiveledclient/MainActivity.kt
+++ app/src/main/java/buoyantair/jonnyfiveledclient/MainActivity.kt
@@ -2,10 +2,17 @@ package buoyantair.jonnyfiveledclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import java.io.Console
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun sendMessage(view: View) {
+        println("Hello world")
+    }
 }
+
